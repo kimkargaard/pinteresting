33dd417e110f5a400e228274ff76a94902c529dd
Kim <kim@datalab101.com>
2014-07-22 16:38:17 +0200

    changed the look and feel and added sign-in page, product page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 get "about" => "pages#about" #About_path
 get "contact" => "pages#contact" #Contact_path
+get "product" => "pages#product" #Product_path
+get "signin" => "pages#signin" #Signin_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
